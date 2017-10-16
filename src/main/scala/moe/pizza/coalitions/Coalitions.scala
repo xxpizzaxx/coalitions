d37dadcbba7d34ad79bc90a90ae41a365631ac0a
Andi Miller <andi@andimiller.net>
2017-10-17 00:49:52 +0100

    start fleshing out coalitions, add names

--- src/main/scala/moe/pizza/coalitions/Coalitions.scala
+++ src/main/scala/moe/pizza/coalitions/Coalitions.scala
@@ -3,7 +3,8 @@ package moe.pizza.coalitions
 object Coalitions {
   import Data._
 
-  val GeminateCo = Coalition(
+  lazy val GeminateCo = Coalition(
+    "GeminateCo",
     List(
       Alliance("Legion of xXDEATHXx", 1411711376),
       Alliance("Infinity Space.", 99003355),
@@ -15,7 +16,8 @@ object Coalitions {
       Alliance("Prothean Alliance", 99005360)
     )
   )
-  val DroneControlUnit = Coalition(
+  lazy val DroneControlUnit = Coalition(
+    "Drone Control Unit",
     List(
       Alliance("Infinity Space.", 99003355),
       Alliance("Ultimate Space", 99003574),
@@ -34,6 +36,96 @@ object Coalitions {
       Alliance("Brothers of Tangra", 99003006)
     )
   )
-  val DRF = Coalition(List(Alliance("SOLAR FLEET", 1208295500)), List.empty, List(GeminateCo, DroneControlUnit))
+  lazy val Legacy = Coalition(
+    "Legacy",
+    List(
+      Alliance("Test Alliance Please Ignore", 498125261),
+      Alliance("Fidelas Constans", 1006830534),
+      Alliance("Brave Collective", 99003214),
+      Alliance("DRONE WALKERS", 99006297),
+      Alliance("Integritas Constans", 99006734)
+    )
+  )
+  lazy val SOLAR = Coalition(
+    "SOLAR",
+    List(
+      Alliance("SOLAR FLEET", 1208295500),
+      Alliance("SOLAR WING.", 99005108)
+    )
+  )
+  lazy val DRF = Coalition(
+    "DRF",
+    List(GeminateCo, DroneControlUnit, Legacy, SOLAR)
+  )
 
+  lazy val Vanguard = Coalition(
+    "Vanguard",
+    List(
+      Alliance("Triumvirate.", 933731581),
+      Alliance("Fraternity.", 99003581),
+      Alliance("League of Unaligned Master Pilots", 99005843),
+      Alliance("Cohortes Triarii", 99005364),
+      Alliance("Deus Vult.", 99006697),
+      Alliance("Destiny's Call", 707482380),
+    )
+  )
+
+  lazy val VanguardKrabs = Coalition(
+    "Vanguard Krabs",
+    List(
+      Alliance("Stella Nova", 99005286),
+      Alliance("Serrice Council.", 99004841),
+      Alliance("Fraternity University", 99006406),
+      Alliance("Ranger Regiment", 99007362),
+      Alliance("Most Usual Suspects", 621338554),
+      Alliance("Destiny's Call", 707482380),
+      Alliance("Brothers Of The Dark Sun", 99003615),
+      Alliance("SAMURAI SOUL'd OUT", 99006138),
+    )
+  )
+
+  lazy val NCPL = Coalition(
+    "NCPL",
+    List(
+      Alliance("Pandemic Legion", 386292982),
+      Alliance("Northern Coalition.", 1727758877),
+    ),
+    List(GOTG, KrabFam)
+  )
+
+  lazy val KrabFam = Coalition(
+    "KrabFam",
+    List(
+      Alliance("Pandemic Horde", 99005338),
+      Alliance("Rate My Ticks", 99007044),
+      Alliance("Here Be Dragons", 1555742183),
+      Alliance("SLYCE Pirates", 99006125),
+      Alliance("Brisingamen", 99006506),
+      Alliance("Denomic Wheat Pineapple", 99006494),
+    )
+  )
+
+  lazy val GOTG = Coalition(
+    "GOTG",
+    List(
+      Alliance("DARKNESS.", 99002938),
+      Alliance("Blades of Grass", 99005393),
+      Alliance("Caladrius Alliance", 99001954),
+      Alliance("ChaosTheory.", 99005955),
+      Alliance("Mistakes Were Made.", 99002208),
+      Alliance("Mordus Angels", 1301367357),
+      Alliance("Solyaris Chtonium", 1042504553),
+      Alliance("Out of Sight.", 99001011),
+    ),
+    List(KrabFam)
+  )
+
+
+  lazy val TheMemeperium = Coalition(
+    "The Memeperium",
+    List(
+      Alliance("I too am Gay", 99006771),
+      Alliance("At Fanfest", 99007734),
+    )
+  )
 }
--- src/main/scala/moe/pizza/coalitions/Data.scala
+++ src/main/scala/moe/pizza/coalitions/Data.scala
@@ -3,8 +3,10 @@ package moe.pizza.coalitions
 object Data {
   case class Alliance(name: String, id: Long)
   case class Corporation(name: String, id: Long)
-  case class Coalition(alliances: List[Alliance], corporations: List[Corporation], coalitions: List[Coalition])
+  case class Coalition(name: String, alliances: List[Alliance], corporations: List[Corporation], coalitions: List[Coalition])
   object Coalition {
-    def apply(as: List[Alliance]): Coalition = apply(as, List.empty, List.empty)
+    def apply(name: String, as: List[Alliance]): Coalition = apply(name, as, List.empty, List.empty)
+    def apply(name: String, cs: List[Coalition]): Coalition = apply(name, List.empty, List.empty, cs)
+    def apply(name: String, as: List[Alliance], cs: List[Coalition]): Coalition = apply(name, as, List.empty, cs)
   }
 }
